b894c3003b6455137b93d92bf29a362e205d9f81
Pascal <arthur.pascal@etu.u-bordeaux.fr>
2023-03-24 10:35:38 +0100

    login fonctionnel

--- symfony/src/Controller/ApiController.php
+++ symfony/src/Controller/ApiController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\DBAL\Schema\View;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Request;use App\Repository\BookRepository;
 use FOS\RestBundle\Controller\Annotations\View as AnnotationsView;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
@@ -21,7 +23,6 @@ class ApiController extends AbstractController
     #[Route('/books', name: 'app_api')]
     public function index(
         EntityManagerInterface $em,
-        SerializerInterface $serializer,
         Request $request,
         BookRepository $book
     ) {
@@ -35,14 +36,67 @@ class ApiController extends AbstractController
         return $books;
     }
 
+    #[AnnotationsView(serializerGroups: ['livre'])]
     #[Route('/books/{nb}', name: 'app_api_nb')]
-    public function getBookByNb(BookRepository $book, int $nb, SerializerInterface $serializer): Response
+    public function getBookByNb(BookRepository $book, int $nb): Response
     {
         $books = $book->findByNb($nb);
-        $context = (new ObjectNormalizerContextBuilder())
-        ->withGroups(['livre'])
-        ->toArray();
-        $json = $serializer->serialize($books, 'json', $context);
-        return $this->json($json);
+        return $books;
+    }
+
+    #[Route('/login', name: 'app_api_login', methods: "POST")]
+    public function login(
+        EntityManagerInterface $em,
+        Request $request,
+        UserRepository $ur,
+        SerializerInterface $serializer
+    ): Response {
+        $credentials = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $ExepectedUser = $ur->findOneByEmail($credentials->getEmail());
+        if ($ExepectedUser == null) {
+            return $this->json([
+                'message' => 'error',
+             ], Response::HTTP_UNAUTHORIZED);
+        }
+        if ($ExepectedUser->getPassword() == $credentials->getPassword()) {
+            $token = rtrim(strtr(base64_encode(random_bytes(32)), '+/', '-'), '=');
+            $ExepectedUser->setToken($token);
+            $em->persist($ExepectedUser);
+            return $this->json([
+                'accessToken' => $token,
+                'user' => $ExepectedUser,
+            ], Response::HTTP_OK);
+        } else {
+            return $this->json([
+                'message' => 'error',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+    }
+    
+    #[Route('/logout', name: 'app_api_logout', methods: "POST")]
+    public function logout(
+        EntityManagerInterface $em,
+        Request $request,
+        UserRepository $ur,
+        SerializerInterface $serializer
+    ): Response {
+        $credentials = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $ExepectedUser = $ur->findOneByEmail($credentials->getEmail());
+        if ($ExepectedUser == null) {
+            return $this->json([
+                'message' => 'error',
+             ], Response::HTTP_UNAUTHORIZED);
+        }
+        if ($ExepectedUser->getPassword() == $credentials->getPassword()) {
+            $ExepectedUser->setToken(null);
+            $em->persist($ExepectedUser);
+            return $this->json([
+                'message' => 'success',
+            ], Response::HTTP_OK);
+        } else {
+            return $this->json([
+                'message' => 'error',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
     }
 }
--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * User
  *
  * @ORM\Table(name="USER")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User
 {
