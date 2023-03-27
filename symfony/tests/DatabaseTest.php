ed24fb1cbfaa58ee78ac345d2032cd7f010c9435
thomrenault <vous@exemple.com>
2023-03-27 10:27:18 +0200

    ajout UserRepository

--- symfony/tests/DatabaseTest.php
+++ symfony/tests/DatabaseTest.php
@@ -10,4 +10,25 @@ class DatabaseTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testEmailAlreadyExist(UserRepository $ur): void
+    {
+        self::bootKernel();
+
+        // Get the Entity Manager
+        $entityManager = self::$container->get('doctrine')->getManager();
+
+        // Create a new User entity
+        $user = new TestUser();
+        $user->setFirstname('Shirley');
+        $user->setLastname('Burke');
+        $user->setEmail('Shirley@gmail.com');
+
+        // Persist the User entity to the database
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Assert that the User entity was saved to the database
+        $this->assertNotNull($user->getId());
+    }
 }
