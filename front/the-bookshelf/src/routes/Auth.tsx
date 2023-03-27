a688ba4f7694d8d0c0258a0de72419a4b148f350
Raphael <raphael.alarcon@etu.u-bordeaux.fr>
2023-03-27 11:28:01 +0200

    🔥 Functionnal authentification

--- front/the-bookshelf/src/routes/Auth.tsx
+++ front/the-bookshelf/src/routes/Auth.tsx
@@ -6,11 +6,24 @@ import { BASE_API_URL } from "@/main";
 function Auth() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    async function loginUser(credentials: { email: string; password: string; }) {
+      return fetch(`${BASE_API_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+      })
+        .then(data => data.json())
+     }
     
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         const response = await loginUser({email, password});
+        
+        console.log(response)
 
         if ('accessToken' in response) {
           swal("Success", response.message, "success", {
@@ -30,16 +43,5 @@ function Auth() {
         <AuthForm setEmail={setEmail} setPassword={setPassword} onSubmit={handleSubmit}/>
     )
 }
-
-async function loginUser(credentials: { email: string; password: string; }) {
-    return fetch(`${BASE_API_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    })
-      .then(data => data.json())
-   }
 
 export default Auth;
