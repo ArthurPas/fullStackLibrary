3475f1e1c349bf99356e27c3684b3e5a502efc42
thomrenault <vous@exemple.com>
2023-03-27 15:55:38 +0200

    fix phpstan

--- symfony/src/Controller/ApiController.php
+++ symfony/src/Controller/ApiController.php
@@ -6,13 +6,11 @@ use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\DBAL\Schema\View;
 use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[View()]
     #[Route('/', name: 'app_api')]
     public function index(EntityManagerInterface $em): Response
     {
