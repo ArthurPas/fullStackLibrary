6098258b05e296694cf71848059180e77a4687ba
Raphael <raphael.alarcon@etu.u-bordeaux.fr>
2023-03-31 10:25:45 +0200

    Modif routes et urls

--- front/the-bookshelf/src/components/nav/NavProfile.tsx
+++ front/the-bookshelf/src/components/nav/NavProfile.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { LogoutButton } from "@/components";
@@ -46,7 +47,7 @@ function NavProfile() {
 			</div>
             { displayMenu ? (
                 <ul className="nav__profile__dropdown">
-                    <h2>
+                    <h5>
                         <img
                             src={JSON.parse(localStorage.getItem("user")!).avatar}
                             alt="Profile"
@@ -55,7 +56,7 @@ function NavProfile() {
                             <p>{JSON.parse(localStorage.getItem("user")!).email}</p>
                             <Link to="/profile" onClick={() => setDisplayMenu(!displayMenu)}>View Profile</Link>
                         </div>
-                    </h2>
+                    </h5>
                     <hr />
                     <LogoutButton />
                 </ul>
--- front/the-bookshelf/src/utils/Constants.ts
+++ front/the-bookshelf/src/utils/Constants.ts
@@ -2,4 +2,4 @@
  * @fileoverview Constants used in the application
  */
 
-export const BASE_API_URL = "https://185.212.225.120:9000/api";
+export const BASE_API_URL = "http://185.212.225.120:9000/api";
